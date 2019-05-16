3d04a2f6e3f4558f0bf349f4a4f2add84ea8251c
fan0521 <fan_nie@hotmail.com>
2019-05-16 11:44:07 -0700

    added file paths and title

--- src/data/split_qual_data.py
+++ src/data/split_qual_data.py
@@ -1,10 +1,26 @@
+# -*- coding: utf-8 -*-
+# Author: Fan Nie
+# Date: May 2019
+# This script file is for splitting the comment datasets into train and test dataset
+
+
 import pandas as pd
 
-# reading in 2018 qualitative data
-df_raw_2018 = pd.read_excel("./data/interim/desensitized_qualitative-data2018.csv",
-                       skiprows = 1)
+# file paths to read the qualitative data
+input_path_2015 = "data/interim/desensitized_qualitative-data2015.csv"
+input_path_2018 = "data/interim/desensitized_qualitative-data2018.csv"
+
+# file paths to write the csv files
+output_path_test_2015 = "data/interim/test_2015-qualitative-data.csv"
+output_path_test_2018 = "data/interim/test_2018-qualitative-data.csv"
+output_path_train_2015 = "data/interim/train_2015-qualitative-data.csv"
+output_path_train_2018 = "data/interim/train_2018-qualitative-data.csv"
+
 # reading in 2015 qualitative data
-df_raw_2015 = pd.read_excel("./data/interim/desensitized_qualitative-data2015.csv")
+df_raw_2015 = pd.read_excel(input_path_2015)
+
+# reading in 2018 qualitative data
+df_raw_2018 = pd.read_excel(input_path_2018,skiprows = 1)
 
 # Put 10% of the data aside for testing and the remaining 90% for training
 df_test_2018 = df_raw_2018.sample(frac = 0.1, random_state = 2019)
@@ -13,8 +29,8 @@ df_train_2015 = df_raw_2015.drop(index = df_test_2015.index)
 df_train_2018 = df_raw_2018.drop(index = df_test_2018.index)
 
 #writing split data to csv files
-df_test_2015.to_csv(path_or_buf = "./data/interim/test_2015-qualitative-data.csv")
-df_test_2018.to_csv(path_or_buf = "./data/interim/test_2018-qualitative-data.csv")
+df_test_2015.to_csv(path_or_buf = output_path_test_2015)
+df_test_2018.to_csv(path_or_buf = output_path_test_2018)
 
-df_train_2015.to_csv(path_or_buf = "./data/interim/train_2015-qualitative-data.csv")
-df_train_2018.to_csv(path_or_buf = "./data/interim/train_2018-qualitative-data.csv")
+df_train_2015.to_csv(path_or_buf = output_path_train_2015)
+df_train_2018.to_csv(path_or_buf = output_path_train_2018)
