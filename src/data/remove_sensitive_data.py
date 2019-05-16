f29bbbc1e791eab6ca37776572d3071eff1494ec
Aaron Quinton <aaron.wquinton@gmail.com>
2019-05-16 11:23:50 -0700

    Updated sensitive script

--- src/data/remove_sensitive_data.py
+++ src/data/remove_sensitive_data.py
@@ -10,15 +10,12 @@ filepath_2015 = "data/raw/WES2015_Final_Qual_Results.xlsx"
 filepath_2018 = "data/raw/2018 WES Qual Coded - Final Comments and Codes.xlsx"
 
 # File Paths to Write the train and test .csv
-filepath_test2015 = "data/interim/test_2015-qaulitative-data.csv"
-filepath_train2015 = "data/interim/train_2015-qaulitative-data.csv"
-filepath_test2018 = "data/interim/test_2018-qaulitative-data.csv"
-filepath_train2018 = "data/interim/train_2018-qaulitative-data.csv"
+filepath_2015 = "data/interim/desensitized_qualitative-data.csv"
+filepath_2018 = "data/interim/desensitized_qualitative-data.csv"
 
 
 ###############################################################################
-# Read in 2015 and 2018 raw qualitative data, remove sensitive comments, and  #
-# prepare it into test and train .csv                                         #
+# Read in 2015 and 2018 raw qualitative data and remove sensitive comments    #
 ###############################################################################
 
 # Read Raw data files from local data directory
@@ -39,19 +36,8 @@ index2018 = sensitive_text.sensitive_index(comments2018)
 df_raw_2018 = df_raw_2018.drop(index=index2018)
 
 
-# Put 10% of the data aside for testing and the remaining 90% for training
-# 2015
-df_test_2015 = df_raw_2015.sample(frac=0.1, random_state=2019)
-df_train_2015 = df_raw_2015.drop(index=df_test_2015.index)
-# 2018
-df_test_2018 = df_raw_2018.sample(frac=0.1, random_state=2019)
-df_train_2018 = df_raw_2018.drop(index=df_test_2018.index)
-
-
 # Write raw train and test dataframes to csv with the sensitive rows removed
 # 2015
-df_test_2015.to_csv(filepath_test2015)
-df_train_2015.to_csv(filepath_train2015)
+df_raw_2015.to_csv(filepath_2015)
 # 2018
-df_test_2018.to_csv(filepath_test2018)
-df_train_2018.to_csv(filepath_train2018)
+df_raw_2018.to_csv(filepath_2018)
