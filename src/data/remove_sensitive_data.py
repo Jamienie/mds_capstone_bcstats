459e9e8ab5a43ea9e9ad361f82a4694227f1383d
Aaron Quinton <aaron.wquinton@gmail.com>
2019-05-19 17:03:05 -0700

    added no index column when writing csv

--- src/data/remove_sensitive_data.py
+++ src/data/remove_sensitive_data.py
@@ -38,6 +38,6 @@ df_raw_2018 = df_raw_2018.drop(index=index2018)
 
 # Write raw train and test dataframes to csv with the sensitive rows removed
 # 2015
-df_raw_2015.to_csv(filepath_out_2015)
+df_raw_2015.to_csv(filepath_out_2015, index=False)
 # 2018
-df_raw_2018.to_csv(filepath_out_2018)
+df_raw_2018.to_csv(filepath_out_2018, index=False)
