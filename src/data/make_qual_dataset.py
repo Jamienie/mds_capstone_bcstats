bf96e3aa3fd1a9eb257de4d7300ee5530be19af2
fan0521 <fan_nie@hotmail.com>
2019-06-03 14:10:22 -0700

    fixed argparse commands

--- src/data/make_qual_dataset.py
+++ src/data/make_qual_dataset.py
@@ -6,20 +6,27 @@
 
 # Dependencies: argparse, pandas, numpy
 
-# Usage: python make_qual_dataset.py "../../data/interim/train_2015-qualitative-data.csv" "../../data/interim/train_2018-qualitative-data.csv" "../../data/interim/qual_combined_train.csv"
+# Usage with default 2015 and 2018 train datasets: python make_qual_dataset.py 
+
+# Usage with 2015 and 2018 test datasets: python make_qual_dataset.py "../../data/interim/test_2015-qualitative-data.csv" "../../data/interim/test_2018-qualitative-data.csv" "../../data/interim/qual_combined_test.csv"
 
 import pandas as pd
 
 import argparse
 
+parser = argparse.ArgumentParser(description='Write csv file for combined 2015 and 2018 qualitative datasets')
+parser.add_argument('--input_2015_csv', '-i2015', type=str, dest="input_2015_qual_interim_filepath", action='store',
+                     default='../../data/interim/train_2015-qualitative-data.csv',
+                     help='input 2015 qual interim filepath')
 
-# read in command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('input_2015_qual_interim_filepath')
-parser.add_argument('input_2018_qual_interim_filepath')
-parser.add_argument('output_combined_qual_csv_filepath')
+parser.add_argument('--input_2018_csv', '-i2018', type=str, dest="input_2018_qual_interim_filepath", action='store',
+                     default='../../data/interim/train_2018-qualitative-data.csv',
+                     help='input 2018 qual interim filepath')
+
+parser.add_argument('--output_csv', '-o', type=str, dest="output_combined_qual_csv_filepath" ,action='store',
+                     default='../../data/interim/qual_combined_train.csv',
+                     help="output combined qual csv filepath")
 args = parser.parse_args()
-
 
 def main():
     
@@ -27,7 +34,9 @@ def main():
    
     # read in 2015 train csv file
     df_2015 = pd.read_csv(args.input_2015_qual_interim_filepath)
+    #df_2015 = pd.read_csv(input_2015)
 
+    
     # convert it to a dataframe
     df_2015 = pd.DataFrame(df_2015)
     df_2015['year_of_survey'] = 2015
@@ -49,9 +58,10 @@ def main():
    
     # readin 2018 train csv file
     df_2018 = pd.read_csv(args.input_2018_qual_interim_filepath)
+    
     # convert it to a dataframe
     df_2018 = pd.DataFrame(df_2018)
-    # df_2018['year_of_survey'] = 2018
+    df_2018['year_of_survey'] = 2018
     df_2018.rename(columns={'2018 Comment':'comments'}, inplace=True)
     # only select the 12 main themes
     df_2018_selected = df_2018[selected_columns]
@@ -63,6 +73,8 @@ def main():
     #print(df_combined.head())
     # write the joined file to a csv files
     df_combined.to_csv(args.output_combined_qual_csv_filepath)
+    
+
 
 # call main function
 if __name__ == "__main__":
