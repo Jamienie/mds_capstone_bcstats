06dbbaef82e54eccadcf5ec4c0d4022c8b652ec2
Ayla Pearson <aylapearson23@gmail.com>
2019-06-22 00:18:36 -0700

    created R script to clean quant data, pushing legends because there have been changes, cleaned up python scripts

--- src/analysis/text_summary.py
+++ src/analysis/text_summary.py
@@ -2,30 +2,47 @@
 """
 Created on Mon Jun  6 11:24:21 2019
 
-@author: payla
+This script preforms text summarization. The generate_text_summary combines 
+all the functions into a single step. The general workflow if you wanted to
+break the analysis up is as follows:
+
+1. Load the data
+2. Clean up grammar to make comments split into more evenly sized sentences
+3. pre-process the text to remove punctutation and stopwords
+4. Create the corpus from individual comments
+5. Generate the summary
+
+The generate_text_summary function can be imported and ran as a function directly
+or it can be called from the command line as a script. 
+
+To run from the command line here is an example:
+
+Navigate to the project root and then run:
+
+python -m src.analysis.text_summary .\data\interim\joined_qual_quant.csv ./references/data-dictionaries/theme_subtheme_names.csv 200 theme Supervisors strong textrank
+
+
 """
 
-import re
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-import nltk
+import re                         # Version 2.2.1
+import numpy as np                # Version 1.16.2
+import pandas as pd               # Version 0.23.4
+import matplotlib.pyplot as plt   # Version 3.0.2
+import nltk                       # Version 3.4
+import argparse                   # Version 1.1
 
 
 from src.data.preprocessing_text import clean_text
 from src.data.preprocessing_text import replace_typical_misspell
+# this might be moved 
+from src.analysis.emotion_analysis import get_theme_labels
 
-# from nltk.corpus import stopwords
-# stop_words = stopwords.words('english')
 
-from gensim.models import KeyedVectors
-import networkx as nx
-from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models import KeyedVectors                  # Version 3.7,1
+import networkx as nx                                   # Version 2.2
+from sklearn.metrics.pairwise import cosine_similarity  # Version 0.20.1
 
-from summa import summarizer
-from gensim.summarization.summarizer import summarize as gensim_summarize
+from gensim.summarization.summarizer import summarize as gensim_summarize # Version 3.7.1
 
 
 
@@ -33,7 +50,8 @@ def generate_corpus_from_comments(data, depth=None, name=None, agreement=None, s
     """
     Filters the data frame down to a specific subtopic or topic and agreement 
     level. If all the defaults are used it will generate a corpus for all the comments
-    present. 
+    present. It also cleans up the grammar in the text so it more evenly splits into
+    sentences. 
     
     Parameters
     ----------
@@ -48,7 +66,7 @@ def generate_corpus_from_comments(data, depth=None, name=None, agreement=None, s
         Either input the name of the main theme (ex: "Executive", "Staffing Practices")
         or the number relating to the subtheme (ex: 12, 43, 102). The default is None
         which means it will do all the
-    agreement: strr'
+    agreement: str
         Either input "all" to see all three levels together or input the level of agreement
         "strong", "weak", "no". Default is set to None which is for comments that have not
         be related to the multiple choice questions
@@ -58,9 +76,15 @@ def generate_corpus_from_comments(data, depth=None, name=None, agreement=None, s
      
     Returns
     -------
+    Returns the combined comments as either a list or string depending on the sentences parameters.
     
-    
-        
+    sentences: list
+        Returns all the combined comments as a list, where each comment is an item in the list. To
+        have the function return this format select True for the sentences parameter.
+    text: str
+        Returns all the combined comments as a sinlge string. To have it returned in this form
+        have the sentences parameter False.
+
     """
 
     data = data.copy()
@@ -93,7 +117,6 @@ def generate_corpus_from_comments(data, depth=None, name=None, agreement=None, s
     # combine list to be a single item
     text = ' '.join(text)
     
-    
     if sentences == True:
         text = corpus_clean_sentences(text)
         sentences = nltk.sent_tokenize(text)
@@ -106,15 +129,18 @@ def generate_corpus_from_comments(data, depth=None, name=None, agreement=None, s
 
 def corpus_clean_sentences(text):
     """
-    Takes a string and cleans it so sentence tokenization works better
+    Takes a string and cleans the grammar so it will more evenly split
+    into sentences.
 
     Parameters
     ----------
     text: str
         Text to be cleaned
+    
     Returns
     -------
     Returns string that is ready for sentence tokenization
+    
     """
 
     # change semi-colon (;) to .
@@ -198,7 +224,6 @@ def preprocess_corpus(text, stop_words=""):
         preprocessed.append(processed_sentences)
 
     return preprocessed
-    
 
 
 def sentence_eda(sentences, word_plot=False, character_plot=False):
@@ -206,6 +231,29 @@ def sentence_eda(sentences, word_plot=False, character_plot=False):
     Input list of sentences and get the mean, median, max, min number of words per sentence.
     The average mean, median, max and min number of characters per sentence.
     If plot = True, it outputs a histogram of the distribution of sentence length
+    
+    Parameters
+    ----------
+    sentences: list
+        List of sentences to have words and characters
+    word_plot: bool
+        If true is selected it will show a plot of the distribution of the
+        number of words per sentences. The default is set to False which means
+        the plot will not display. 
+    character_plot: bool
+        If true is selected it will show a plot of the distribution of the
+        number of characters per sentence. The default is set to False which means
+        the plot will not display.
+    
+    Returns
+    -------
+    Displays word plot and character plot if selected and returns a dataframe with
+    summary statistics.
+    
+    stats_df: dataframe
+        Dataframe with the min, max, mean and median for the number of words and
+        characters. 
+   
     """
     # count the number of characters per sentence
     counts_char = []
@@ -249,25 +297,29 @@ def sentence_eda(sentences, word_plot=False, character_plot=False):
     d = {'stats' : labels,
          'character values': values_char,
          'word values': values_word}
-    df = pd.DataFrame(data=d).round(2)
+    stats_df = pd.DataFrame(data=d).round(2)
 
-    return df
+    return stats_df
 
 
 
 def load_word_embeddings(file_path):
     """
+    Returns the loaded embeddings from the downloaded file
     
-    
-    Parameters:
+    Parameters
+    ----------
     file_path: str
         The file path to the downloaded embeddings 
-    
+    Returns
+    -------
+    loaded_embedding: gensim.models.keyedvectors.Word2VecKeyedVectors
+        Returns the embeddings loaded as a gensim object
     
     """
     
-    embedd = KeyedVectors.load_word2vec_format(file_path)
-    return embedd
+    loaded_embedding = KeyedVectors.load_word2vec_format(file_path)
+    return loaded_embedding
 
 
 
@@ -320,13 +372,10 @@ def generate_summary_pagerank_pretrained_embedding(text, embedding, embedding_si
         # if the sentence is blank just give it all zeros
         else:
             v = np.zeros((embedding_size,))
-
         sentence_vectors.append(v)  
-        
-        
+
            # similarity matrix
     sim_mat = np.zeros([len(text), len(text)])
-
     # add cosine similarity scores to the matrix
     for i in range(len(text)):
         for j in range(len(text)):
@@ -340,47 +389,25 @@ def generate_summary_pagerank_pretrained_embedding(text, embedding, embedding_si
    # sort by score
     ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(text)), reverse=True)
     # output top n sentences
-    
     top_n = []
     for i in range(size_summary):
         top_n.append(ranked_sentences[i][1])
-    
     return top_n 
         
-        
-def generate_summary_summa(text, size_summary=200):
-    """
-    Performs text summary from summa summarizer
-    
-    Parameters
-    ----------
-    text: list
-        A list of sentences
-    
-   
-    """
-    
-    # still need to cross references it back to pre-preproccess so it has better grammar
-    # but good enough for now
-    
-    text = preprocess_corpus(text)
-    text = ". ".join(text)
-    
-    return summarizer.summarize(text, words=size_summary, split=True)
-    
-    
-    
-    
-    
+      
 def generate_summary_gensim(text, size_summary=200):
     """
     Performs text summary from gensim summarizer 
     
-    
     Parameters
     ----------
     text: list
         A list of sentences
+    
+    Returns
+    -------
+    summary: list
+        List with each item being a single sentence
     
     """
     
@@ -388,10 +415,219 @@ def generate_summary_gensim(text, size_summary=200):
     text = ". ".join(text)
     
     return gensim_summarize(text, word_count=size_summary, split=True)
+
+
+def generate_text_summary(input_file, legend_file, summary_size, depth, name, agreement, 
+                  method, embedding_file=None, embedding=None, embedding_return=False,
+                         output_file="./data/processed/"):
+    """
+    Input the data file and this function will clean, process and summarize the comments. 
+    This funcation can summarize all the text or it split to subtheme/theme and agreement
+    level. It will also generate an output file of a csv with the summary sentences. 
+    
+    This function can perform text sumarization with different algorithms one using PageRank 
+    and the other using TextRank. The method can be choosen under the methods parameter.
+    
+    The pagerank method has been implemented locally and all the code is present in this 
+    module. This method is much slower, it has been included still because all the code
+    is in the module and can be easily understood. To use this method you can save the 
+    loaded embeddings after the first time it has been run if you select the correct parameters.
+    This can greatly decrease the time it takes to run. Examples are present below. 
+    
+    The textrank method is from the Gensim package. It uses a variation on textrank where
+    the similarity method is using BM25 similarity. Since this implementation is from
+    a package the code is not present in this module. 
+    
+    Parameters
+    ----------
+    input_file: str
+        Path to the input data
+    legend_file: str
+        Path to the legend containing the mapping between subtheme codes and themes
+    summary_size:
+        For pretrained embedding it is the number of sentences
+        For summa and gensim it is the number of characters  
+    depth: str
+        Either theme, subtheme or None
+    name: str or integer
+        Either input the name of the main theme (ex: "Executive", "Staffing Practices")
+        or the number relating to the subtheme (ex: 12, 43, 102). The default is None
+        which means it will do all the
+    agreement: str
+        Either input "all" to see all three levels together or input the level of agreement
+        "strong", "weak", "no". Default is set to None which is for comments that have not
+        be related to the multiple choice questions
+    method: str
+        Select the summarization method to be used: "pagerank" or "textrank". The pagerank 
+        method uses pre-trained embeddings and is a slower method then textrank. 
+    output_file: str
+        Path to where the summary file will be written out too. The default writes 
+        a file to the processed folder within data. If None is choosen then it will
+        not write a file.
+    embedding_file: str
+        Path to the file containing the word embeddings
+    embedding: gensim.models.keyedvectors.Word2VecKeyedVectors
+        Loaded word vectors, this can help to save time once you have loaded them embeddings
+    embedding_return: bool
+        If set to true the function will return the loaded embeddings. It will still
+        output the summary.
+        
+    Returns
+    -------
+    It prints the summary to screen and returns it. If embedding_return is true it also 
+    returns the loaded embedding. 
+    
+    Summary: list
+        the items are the sentence
+    embedding: gensim.models.keyedvectors.Word2VecKeyedVectors
+        If embedding_return == True it returns the summary and the loaded vector
+    
+    Examples
+    --------
+    # load and save the pretrained embeddings
+    # geneate summary with 5 sentences for subtheme 13 with weak agreement
+    summary, loaded_embedding = generate_text_summary(".\data\interim\joined_qual_quant.csv",  
+                                        "./references/data-dictionaries/theme_subtheme_names.csv",
+                                        5,
+                                        "subtheme",
+                                        13,
+                                        "weak",
+                                        "pagerank",
+                                        "./references/pretrained_embeddings.nosync/fasttext/crawl-300d-2M.vec",
+                                        embedding_return=True)
+    
+    # now use the loaded embeddings to speed up the run time
+    generate_text_summary(".\data\interim\joined_qual_quant.csv",  
+                                    "./references/data-dictionaries/theme_subtheme_names.csv",
+                                    5,
+                                    "subtheme",
+                                    13,
+                                    "weak",
+                                    "pagerank",
+                                    embedding=loaded_embedding)
     
     
+    # to generate a summary with 200 characters for subtheme 13 with weak agreement
+    generate_text_summary(".\data\interim\joined_qual_quant.csv",  
+                                        "./references/data-dictionaries/theme_subtheme_names.csv",
+                                        200,
+                                        "subtheme",
+                                        13,
+                                        "weak",
+                                        "textrank")
+                                        
+    # generate a summary 100 characters long for theme Supervisors with strong agreement
+    generate_text_summary(".\data\interim\joined_qual_quant.csv",  
+                                        "./references/data-dictionaries/theme_subtheme_names.csv",
+                                        100,
+                                        "theme",
+                                        Supervisors,
+                                        "strong",
+                                        "textrank")
     
     
+    # generate a summary 100 characters long for theme Supervisors with strong agreement but choose 
+    # to not have it write  a csv 
+    generate_text_summary(".\data\interim\joined_qual_quant.csv",  
+                                        "./references/data-dictionaries/theme_subtheme_names.csv",
+                                        100,
+                                        "theme",
+                                        Supervisors,
+                                        "strong",
+                                        "textrank",
+                                        output_file=None)
+        
+    """
+    # read in data and legend
+    data = pd.read_csv(input_file, usecols = [0, 1, 4, 5, 6])
+    legend = pd.read_csv(legend_file)
+    # add theme labels to dataset
+    data = get_theme_labels(data, legend)
+    # generate corpus
+    corpus = generate_corpus_from_comments(data,
+                                           depth=depth, 
+                                           name=name, 
+                                           agreement=agreement, 
+                                           sentences=True)
+    # PageRank with word embeddings
+    if method == "pagerank":
+        if isinstance(embedding_file, str) == True:
+            print("loading embeddings, this will take a few mins")
+            # load embeddings
+            embedding_tr = load_word_embeddings(embedding_file)
+            embed = embedding_tr   
+        if embedding != None:
+            embed = embedding 
+        # generate summary     
+        summary = generate_summary_pagerank_pretrained_embedding(corpus, 
+                                                         embed, 
+                                                         embedding_size=300, 
+                                                         size_summary=summary_size)
+    if method == "textrank":
+        summary = generate_summary_gensim(corpus, summary_size)
+    if depth == None:
+        if agreement == None: 
+            title = "Summary for 2018 WES"
+        else:
+            title = "Summary for 2018 WES"  +\
+            " - " + str(agreement).title() +" Agreement"
+    if depth == "subtheme":
+        if agreement == None:
+            title = "Summary for " +\
+            str(legend[legend["code"] == name].iloc[0]["subtheme_description"]).title() 
+        else:
+            title = "Summary for " +\
+            str(legend[legend["code"] == name].iloc[0]["subtheme_description"]).title()  +\
+            " - " + str(agreement).title() +" Agreement"
+    if depth == "theme":
+        if agreement == None:
+            title = "Summary for " + str(name).title()  
+        else:
+            title = "Summary for " + str(name).title() + " - " + str(agreement).title() +" Agreement"
+       
+    # print summary to screen 
+    print(title)
+    print("--------------------------------------------------------------------------")
+    for i in summary:
+        print(i, "\n")
+        
+    if output_file != None:
+        output_file_with_name = output_file + "summary_" + str(name) + ".csv"
+        df_write = pd.DataFrame(summary, columns=["summary"])
+        df_write["theme/subtheme"] = name
+        df_write.to_csv(output_file_with_name, index=False)
+        
+    if embedding_return == True:
+        return summary, embedding_tr 
+    
+    return summary
     
     
-    
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("legend_file", type=str)
+    parser.add_argument("summary_size", type=int)
+    parser.add_argument("depth", type=str)
+    parser.add_argument("name", type=str)
+    parser.add_argument("agreement", type=str)
+    parser.add_argument("method", type=str)
+    parser.add_argument("--embedding_file", default=None)
+    parser.add_argument("--embedding", default=False)
+    parser.add_argument("--embedding_return", default=False)
+    parser.add_argument("--output_file", default="./data/processed/")
+    args = parser.parse_args() 
+
+    generate_text_summary(args.input_file, 
+                          args.legend_file, 
+                          args.summary_size, 
+                          args.depth, 
+                          args.name, 
+                          args.agreement, 
+                          args.method, 
+                          embedding_file=args.embedding_file, 
+                          embedding=args.embedding, 
+                          embedding_return=args.embedding_return,
+                          output_file=args.output_file)
